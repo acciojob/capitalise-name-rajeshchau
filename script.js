b0472606dc3ecb0ae82596e3f5153043d1efb058
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-12 13:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//your JS code here. If required.
 function upperCase() {
   const x = document.getElementById("fname");
   x.value = x.value.toUpperCase();
 }
-
-upperCase()
+document.getElementById("lname").addEventListener("change",upperCase())
